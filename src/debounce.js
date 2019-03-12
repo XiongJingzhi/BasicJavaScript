2819d43673407ff26384af781ca3c31db91d7149
xiongjingzhi <davidxiong1994@gmail.com>
2019-03-12 15:39:37 +0800

    'add'

---
+++ src/debounce.js
@@ -0,0 +1,96 @@
+// 触发事件，一定在wait之后执行，如果wait内又触发，则以下一次事件时间为准
+
+// 防抖函数 v0.1， 效果 wait内 不再触发，才触发时间，不然重新计时
+function debounce(func, wait) {
+  let timeout
+  return function() {
+    clearTimeout(timeout)
+    timeout = setTimeout(func, wait)
+  }
+}
+
+// v0.2 this指针
+function debounce(func, wait) {
+  let timeout
+  return function() {
+    clearTimeout(timeout)
+    timeout = setTimeout(func.bind(this), wait)
+  }
+}
+
+// v0.3 绑定事件参数
+function debounce(func, wait) {
+  let timeout
+  return function() {
+    clearTimeout(timeout)
+    timeout = setTimeout(func.bind(this, ...arguments), wait)
+  }
+}
+
+// v0.4 立刻执行函数，效果如果立即启动，则wait以后，才能第二次触发
+function debounce(func, wait, immediate) {
+  // 通过闭包，把timeout缓存
+  let timeout
+  return function() {
+    if (timeout) clearTimeout(timeout)
+    if (immediate) {
+      // wait内已经触发了，不再执行
+      if (!timeout) {
+        func.apply(this, arguments)
+      }
+      // 设置超时时间, 超时则清掉timeout
+      timeout = setTimeout(function() {
+        timeout = null
+      }, wait)
+    } else {
+      timeout = setTimeout(func.bind(this, ...arguments), wait)
+    }
+  }
+}
+
+// v0.5 带返回值
+function debounce(func, wait, immediate) {
+  let timeout, result
+  return function() {
+    if (timeout) clearTimeout(timeout)
+    if (immediate) {
+      // wait内已经触发了，不再执行
+      if (!timeout) {
+        result = func.apply(this, arguments)
+      }
+      // 设置超时时间, 超时则清掉timeout
+      timeout = setTimeout(function() {
+        timeout = null
+      }, wait)
+    } else {
+      timeout = setTimeout(func.bind(this, ...arguments), wait)
+    }
+    return result
+  }
+}
+
+// v0.6 取消触发
+function debounce(func, wait, immediate) {
+  let timeout, result
+  var debounced = function() {
+    if (timeout) clearTimeout(timeout)
+    if (immediate) {
+      // wait内已经触发了，不再执行
+      if (!timeout) {
+        result = func.apply(this, arguments)
+      }
+      // 设置超时时间, 超时则清掉timeout
+      timeout = setTimeout(function() {
+        timeout = null
+      }, wait)
+    } else {
+      timeout = setTimeout(func.bind(this, ...arguments), wait)
+    }
+    return result
+  }
+  debounced.cancel = function() {
+    clearTimeout(timeout)
+    timeout = null
+  }
+  return debounced
+}
---
+++ src/throttle.js
@@ -0,0 +1,108 @@
+// 原理： 如果你持续触发事件，每隔一段时间，只执行一次事件。
+
+// v1 时间戳, 第一次会立刻执行，第二次事件wait后执行，停止触发后不会再执行
+function throttle(func, wait) {
+  let context, args
+  let previous = 0
+  return function() {
+    // 第二次进入是，如果 previous 为上次的now时间，需要一直超过时间，才能触发
+    const now = +new Date()
+    context = this
+    args = arguments
+    if (now - previous > wait) {
+      func.apply(context, args)
+      previous = now
+    }
+  }
+}
+
+// v2 使用定时器， 第一次经过wait时间后才会执行，第二次wait后执行，停止触发后还会执行一次
+function throttle(func, wait) {
+  let timeout
+  return function() {
+    const context = this
+    const args = arguments
+    if (!timeout) {
+      timeout = setTimeout(function() {
+        timeout = null
+        func.apply(context, args)
+      }, wait)
+    }
+  }
+}
+
+// v3 鼠标移入能立刻执行，停止触发的时候还能再执行一次
+function throttle(func, wait) {
+  let timeout, context, args
+  let previous
+
+  const later = function() {
+    previous = +new Date()
+    timeout = null
+    func.apply(context, args)
+  }
+
+  var throttled = function() {
+    // 每次进入，先检测剩余时间
+    const now = +new Date()
+    const remaining = wait - (now - previous)
+    const context = this
+    const args = arguments
+    // 如果没有剩余的时间了或者你改了系统时间
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout)
+        timeout = null
+      }
+      previous = now
+      func.apply(context, args)
+    } else if (!timeout) {
+      timeout = setTimeout(later, remaining)
+    }
+  }
+  return throttled
+}
+
+// 第四版
+// leading：false 表示禁用第一次执行
+// trailing: false 表示禁用停止触发的回调
+
+function throttle(func, wait, options) {
+  var timeout, context, args
+  var previous = 0
+  if (!options) options = {}
+
+  var later = function() {
+    previous = options.leading === false ? 0 : new Date().getTime()
+    timeout = null
+    func.apply(context, args)
+    if (!timeout) context = args = null
+  }
+
+  var throttled = function() {
+    var now = new Date().getTime()
+    if (!previous && options.leading === false) previous = now
+    var remaining = wait - (now - previous)
+    context = this
+    args = arguments
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout)
+        timeout = null
+      }
+      previous = now
+      func.apply(context, args)
+      if (!timeout) context = args = null
+    } else if (!timeout && options.trailing !== false) {
+      timeout = setTimeout(later, remaining)
+    }
+  }
+
+  throttled.cancel = function() {
+    clearTimeout(timeout)
+    previous = 0
+    timeout = null
+  }
+
+  return throttled
+}
